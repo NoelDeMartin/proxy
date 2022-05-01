6efa67662fb27add360f5a8d6ab2c4fe62dbf5bd
Noel De Martin <noel@noeldemartin.com>
2022-05-01 16:49:52 +0200

    Fix chown script

--- scripts/chown.sh
+++ scripts/chown.sh
@@ -2,4 +2,10 @@
 
 WWWDATA_UID=`docker-compose -f docker-compose.prod.yml run app id -u www-data | tail -n 1 | sed 's/\r$//'`
 
+if [ -z $WWWDATA_UID ]; then
+    echo "Could not set permissions"
+
+    exit 1
+fi
+
 sudo chown -R $WWWDATA_UID:docker storage
