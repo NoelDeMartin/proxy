354f20efaeb7184c9c32762f253368829eace9e6
Noel De Martin <noel@noeldemartin.com>
2026-09-03 16:32:01 +0200

    Configure type checks

--- app/Http/Controllers/FetchController.php
+++ app/Http/Controllers/FetchController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProxyRequest;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 
 class FetchController extends Controller
 {
-    public function __invoke(ProxyRequest $request)
+    public function __invoke(ProxyRequest $request): Response
     {
-        return Http::get($request->get('url'));
+        return Http::get($request->string('url'));
     }
 }
